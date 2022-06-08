6c7d6111401544c9c152ebc9490e79ddf0f81689
Dawid Pawulski <dpawulski@gmail.com>
2022-06-08 14:07:42 +0200

    End of section 20

--- client-app/src/features/activities/dashboard/ActivityListItemAttendee.tsx
+++ client-app/src/features/activities/dashboard/ActivityListItemAttendee.tsx
@@ -11,7 +11,10 @@ interface Props {
 }
 
 export default observer(function ActivityListItemAttendee({ attendees }: Props) {
-    const { } = useStore();
+    const styles = {
+        borderColor: 'orange',
+        borderWidth: 3
+    }
 
     return (
         <List horizontal>
@@ -21,7 +24,13 @@ export default observer(function ActivityListItemAttendee({ attendees }: Props) 
                     key={attendee.username}
                     trigger={
                         <List.Item key={attendee.username} as={Link} to={`/profiles/${attendee.username}`}>
-                            <Image size='mini' circular src={attendee.image || '/assets/user.png'} />
+                            <Image 
+                                size='mini' 
+                                circular 
+                                src={attendee.image || '/assets/user.png'} 
+                                bordered
+                                style={attendee.following ? styles : null}
+                            />
                         </List.Item>
                     }
                 >
